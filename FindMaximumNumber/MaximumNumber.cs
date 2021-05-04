4c910494d4a0d93d205f908fc10f8d36be52fbe3
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-04 15:11:50 +0530

    [prashant]Maximum integer is done

--- FindMaximumNumber/MaximumNumber.cs
+++ FindMaximumNumber/MaximumNumber.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace FindMaximumNumber
 {
-    class MaximumNumber
+   public class MaximumNumber
     {
-
+        public static int Maximum(int firstNumber,int secondtNumber,int thirdtNumber)
+        {
+           
+                if (firstNumber.CompareTo(secondtNumber) > 0 && firstNumber.CompareTo(thirdtNumber) > 0)
+                {
+                    return firstNumber;
+                }
+                else if (secondtNumber.CompareTo(firstNumber) > 0 && secondtNumber.CompareTo(thirdtNumber) > 0)
+                {
+                    return secondtNumber;
+                }
+                else if (thirdtNumber.CompareTo(secondtNumber) > 0 && thirdtNumber.CompareTo(firstNumber) > 0)
+                {
+                    return thirdtNumber;
+                }
+                throw new Exception("firstNumber,secondNumber,ThirdNumber are same");
+            
+        }
     }
 }
